e21d083d498b358e21a39fa3c07d83c99b4e346e
WACHIRAWIT TANASINSAWAT <wachirawit.tanasinsawat@ibm.com>
2019-05-30 14:52:42 +0700

    init lab

---
+++ lab_2/php_project/index.php
@@ -0,0 +1,9 @@
+<html>
+    <head>
+        <title>Web Application for Docker Lab</title>
+        <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+    </head>
+    <body>
+        <h1>Hello World! using PHP Web</h1>
+    </body>
+</html>
